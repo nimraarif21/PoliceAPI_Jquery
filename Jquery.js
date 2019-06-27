8164ee7a044d40141b8e16f0d80e3b94826a3ac0
Nimra Arif <nimra.arif@tintash.com>
2019-06-27 14:14:25 +0500

    Version 2

--- Jquery.js
+++ Jquery.js
@@ -1,16 +1,38 @@
 
 forces=[]
 
+$(document).ready(function(){
+    $('body').on('click', '.parent-row', function(event){
+        $(this).next().toggleClass('show')
+    });
+
+
+    // $('parent-row').click(function(event){
+    //     event.target
+    //     $(this).next().toggleClass('show')
+    // });
+});
+
+
 $(function(){
     All_Forces();
     forces=$.parseJSON(localStorage.getItem('forces'))
+    $('#container').append(
+        '<h1>UK POLICE STATS</h1>'
+    )
+    $('#myList').append(
+        '<thead><tr><th>Police Name</th><th>Police ID</th></tr></thead>' + 
+        '<tbody></tbody>'
+    )
     $.each(
         forces,
         function(i,value){
             force_id=forces[i]['id'];
-            Senior_Officers(force_id);
+            force_name=forces[i]["name"]
             Specific_Force(force_id);
-
+            Show_Force(force_id,force_name,i);
+            Senior_Officers(force_id);
+            ShowSeniorOfficers(force_id,force_name)
         }
         )
 })
@@ -18,7 +40,7 @@ $(function(){
 function All_Forces(){
     if('forces' in localStorage){
         var local_all_forces = $.parseJSON(localStorage.getItem('forces'))
-        ShowAllForces(local_all_forces)
+        return local_all_forces
     }
     else {
         $.ajax({
@@ -27,16 +49,17 @@ function All_Forces(){
             success: function(data){
                 local_all_forces = data
                 localStorage.setItem('forces', JSON.stringify((data)))
-                ShowAllForces(local_all_forces)
+               return local_all_forces
             }
         })
     }
+
 }
 
 function Specific_Force(PoliceID){
     if(PoliceID in localStorage){
         specific_force = $.parseJSON(localStorage.getItem(PoliceID))
-        ShowSpecificForce(PoliceID,specific_force)
+        return specific_force
     }
     else {
         $.ajax({
@@ -45,7 +68,7 @@ function Specific_Force(PoliceID){
             success: function(data){
                 specific_force = data
                 localStorage.setItem(PoliceID, JSON.stringify((data)))
-                ShowSpecificForce(PoliceID,specific_force)
+                return specific_force
             }
         })
     }
@@ -53,18 +76,16 @@ function Specific_Force(PoliceID){
 
 function Senior_Officers(PoliceID){
     if(PoliceID+'/people' in localStorage){
-        senior_officers = $.parseJSON(localStorage.getItem(PoliceID+'/people'))
-        ShowSeniorOfficers(PoliceID,senior_officers)
+        return $.parseJSON(localStorage.getItem(PoliceID+'/people'))
     }
     else {
         $.ajax({
             type: 'GET',
-
             url: 'https://data.police.uk/api/forces'+PoliceID+'/people',
             success: function(data){
                 senior_officers = data
                 localStorage.setItem(PoliceID+'/people', JSON.stringify((data)))
-                ShowSeniorOfficers(PoliceID,senior_officers)
+                return senior_officers;
             }
         })
     }
@@ -72,23 +93,15 @@ function Senior_Officers(PoliceID){
 
 
 
-function ShowAllForces(data){
-    $.each(
-        data,
-        function(i, list){
-            forces.push(data[i]['id'])
-        }
-    )
+function Show_Force(Pid,name,id){
 
-    $('#myList').append(
-        '<thead><tr><th>Police Name</th><th>Police ID</th></tr></thead>' + 
-        '<tbody></tbody>'
-    )
-    $.each(data, function(id, list){
-        $('#myList tbody').append(
-            '<tr><td>' + list['name'] + '</td> <td>' + list['id'] + '</td></tr>'
+        $('#myList').append(
+            '<tr class="parent-row"></div>");><td>' 
+            + name + '</td> <td>' + Pid + '</td></tr>'
         )
-    })
+    
+        ShowSpecificForce(Pid,id)
+
     $(document).ready( function () {
         $('#myList').DataTable();
     } );
@@ -96,16 +109,17 @@ function ShowAllForces(data){
     
 }
 
-function ShowSeniorOfficers(PoliceID, data)
+function ShowSeniorOfficers(PoliceID, PoliceName)
 { 
-
+    data = $.parseJSON(localStorage.getItem(PoliceID+'/people'))
+    $('#myList2').append('<h3>Details of Senior Officers of '+PoliceName+'</h3>')
     $('#myList2').append(
-        '<thead><h1>'+PoliceID+'/<h1><tr><th>SeniorOfficerName</th><th>Bio</th><th>Rank</th></tr></thead>' + 
+        '<thead><tr><th>Senior Officer Name</th><th>Rank</th></tr></thead>' + 
         '<tbody></tbody>'
     )
     $.each(data, function(id, list){
         $('#myList2 tbody').append(
-            '<tr><td>' + list['name'] + '</td> <td>' + list['bio'] + '</td> <td>' + list['rank'] + '</td></tr>'
+            '<tr><td>' + list['name'] + '</td> <td>' + list['rank'] + '</td></tr>'
         )
     })
     $(document).ready( function () {
@@ -116,18 +130,30 @@ function ShowSeniorOfficers(PoliceID, data)
 }
 
 
-function ShowSpecificForce(PoliceID,data){
+function ShowSpecificForce(PoliceID,id){
 
-    $('#myList1').append(
-        '<thead><h1>'+PoliceID+'/<h1><tr><th>NAME</th><th>URL</th><th>DESCRIPTION</th></tr></thead>' + 
-        '<tbody></tbody>'
-    )
-    $('#myList1 tbody').append(
-        '<tr><td>' + data['name'] + '</td> <td>' + data['url'] + '</td> <td>' + data['description'] + '</td></tr>'
-    )
+    specific_force = $.parseJSON(localStorage.getItem(PoliceID))
+  
+    $('#myList').append(
+        '<tr id='+id+' class="collapse"> <td colspan="6" class="hiddenRow"><div><h2>URL :</h2><p>'+specific_force["url"]+'</p><h2>TELEPHONE :</h2><p>'+specific_force["telephone"]+'</p><h2>DESCRIPTION : </h2><p>'+specific_force["description"]+'</p></div></td></tr>');
+     
+    if (specific_force["description"!=null])
+    {
+        $('#myList').append(
+            '<tr id='+id+' class="collapse"> <td colspan="6" class="hiddenRow"><div><h2>URL :</h2><p>'+specific_force["url"]+'</p><h2>TELEPHONE :</h2><p>'+specific_force["telephone"]+'</p><h2>DESCRIPTION : </h2><p>'+specific_force["description"]+'</p></div></td></tr>');
+         
+    }
+    else
+    {
+        $('#myList').append(
+            '<tr id='+id+' class="collapse"> <td colspan="6" class="hiddenRow"><div><h2>URL :</h2><p>'+specific_force["url"]+'</p><h2>TELEPHONE :</h><p>'+specific_force["telephone"]+'</p></div></td></tr>');
+         
+    }
     $(document).ready( function () {
         $('#myList1').DataTable();
     } );
     
 
 }
+
+  
